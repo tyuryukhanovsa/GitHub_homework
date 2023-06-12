a46d5859d31f86c2a7a0cf91998b1805564f4327
Sergey <serega.tiuriuhanov94@gmail.com>
2023-06-12 21:55:17 +0300

    add new function "capital_first"

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def capital(letters):
     return letters.upper()
+
+
+def capital_first(letters):
+    """
+    делает заглавными первые буквы
+    каждого слова в строке
+    """
+    return letters.title()
